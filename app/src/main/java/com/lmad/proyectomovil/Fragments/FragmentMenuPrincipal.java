451984ff4b07da9bc045138f21558f4999d727a3
Mario <mario_i.g@hotmail.com>
2016-10-22 23:10:11 -0500

    Creación fragmentos

---
+++ app/src/main/java/com/lmad/proyectomovil/Fragments/FragmentMenuPrincipal.java
@@ -0,0 +1,23 @@
+package com.lmad.proyectomovil.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lmad.proyectomovil.R;
+
+/**
+ * Created by Mario on 22/10/2016.
+ */
+
+public class FragmentMenuPrincipal extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.menu_principal, container, false);
+        return rootView;
+    }
+}
--- app/src/main/java/com/lmad/proyectomovil/MainActivity.java
+++ app/src/main/java/com/lmad/proyectomovil/MainActivity.java
@@ -1,13 +1,68 @@
 package com.lmad.proyectomovil;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.lmad.proyectomovil.Fragments.FragmentListaFavoritos;
+import com.lmad.proyectomovil.Fragments.FragmentLogin;
+import com.lmad.proyectomovil.Fragments.FragmentMenuPrincipal;
+import com.lmad.proyectomovil.Fragments.FragmentPerfil;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout =(DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_viewer);
+
+        changeFragment(new FragmentMenuPrincipal(),"menu");
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id =item.getItemId();
+                if (id==R.id.navPerfil){
+                    changeFragment(new FragmentPerfil(),"perfil");
+                } else if(id==R.id.navFavoritos){
+                    changeFragment(new FragmentListaFavoritos(),"favoritos");
+                } else if(id==R.id.navCerrarSesion){
+                    changeFragment(new FragmentLogin(),"login");
+                }
+                //cerrar el navigation
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+    }
+
+    private void changeFragment(Fragment fragment, String tag) {
+        FragmentManager fm = getSupportFragmentManager();
+
+        Fragment currentFragment = fm.findFragmentByTag(tag);
+
+        if (currentFragment != null && currentFragment.isVisible()){
+            Toast.makeText(this, "¡Ya está abierto", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        FragmentTransaction ft= fm.beginTransaction(); //abrir una transicion (agregar, quitar o reemplazar)
+
+        //ft.addToBackStack(null); //no regresar al último fragmento
+        ft.replace(R.id.frame_container, fragment, tag); //(id, fragmento)
+
+        ft.commit();//cerrar conexión
     }
 }
