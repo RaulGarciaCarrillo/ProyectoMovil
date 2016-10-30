2f7da2a41a1415626d9246b1449dcca95b07c552
Raul <ragc_96@hotmail.com>
2016-10-30 14:57:37 -0600

    Clases comentario y usuario

---
+++ app/src/main/java/com/lmad/proyectomovil/model/Comentario.java
@@ -0,0 +1,12 @@
+package com.lmad.proyectomovil.model;
+
+/**
+ * Created by Raul on 30/10/2016.
+ */
+
+public class Comentario {
+    String comentario;
+
+
+
+}
---
+++ app/src/main/java/com/lmad/proyectomovil/model/Usuario.java
@@ -0,0 +1,8 @@
+package com.lmad.proyectomovil.model;
+
+/**
+ * Created by Raul on 30/10/2016.
+ */
+
+public class Usuario {
+}
