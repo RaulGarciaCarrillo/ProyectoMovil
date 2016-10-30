59d7f46e8e423fa32e1d8c9a71afb64856fb47c2
Raul <ragc_96@hotmail.com>
2016-10-30 15:46:56 -0600

    Clase usuario

--- app/src/main/java/com/lmad/proyectomovil/model/Usuario.java
+++ app/src/main/java/com/lmad/proyectomovil/model/Usuario.java
@@ -5,4 +5,58 @@ package com.lmad.proyectomovil.model;
  */
 
 public class Usuario {
+    String id;
+    String apodo;
+    String correo;
+    String contrasenia;
+    String foto;
+    String activo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getActivo() {
+        return activo;
+    }
+
+    public void setActivo(String activo) {
+        this.activo = activo;
+    }
 }
