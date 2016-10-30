933f8157f7a450014fa98a79390b5d7ef3856ffb
Raul <ragc_96@hotmail.com>
2016-10-30 15:46:38 -0600

    Clase comentario

--- app/src/main/java/com/lmad/proyectomovil/model/Comentario.java
+++ app/src/main/java/com/lmad/proyectomovil/model/Comentario.java
@@ -6,7 +6,30 @@ package com.lmad.proyectomovil.model;
 
 public class Comentario {
     String comentario;
+    Usuario usuario;
+    Puesto puesto;
 
+    public String getComentario() {
+        return comentario;
+    }
 
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
 
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Puesto getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(Puesto puesto) {
+        this.puesto = puesto;
+    }
 }
