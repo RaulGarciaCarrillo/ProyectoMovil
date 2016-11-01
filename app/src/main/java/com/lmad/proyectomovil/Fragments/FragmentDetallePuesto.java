833872bdadbf35601d62ed4f34ca034cca1fcf85
Raul <ragc_96@hotmail.com>
2016-10-31 19:26:19 -0600

    Pantalla detalle puesto

--- app/src/main/java/com/lmad/proyectomovil/Fragments/FragmentDetallePuesto.java
+++ app/src/main/java/com/lmad/proyectomovil/Fragments/FragmentDetallePuesto.java
@@ -7,12 +7,15 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Base64;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.lmad.proyectomovil.R;
@@ -40,6 +43,8 @@ public class FragmentDetallePuesto extends Fragment{
     Button btnPost;
     EditText editComment;
 
+    ScrollView scrollPuesto;
+
 
     @Nullable
     @Override
@@ -51,6 +56,7 @@ public class FragmentDetallePuesto extends Fragment{
         lstComennts = (ListView) rootView.findViewById(R.id.lstComennts);
         btnPost = (Button) rootView.findViewById(R.id.btnPost);
         editComment = (EditText) rootView.findViewById(R.id.editComment);
+        scrollPuesto = (ScrollView) rootView.findViewById(R.id.scrollPuesto);
 
         new Networking(rootView.getContext()).execute("cargarDetallePuesto", puesto,new MyCallback() {
             @Override
@@ -79,6 +85,7 @@ public class FragmentDetallePuesto extends Fragment{
                         if (comentarioList.size() != 0) {
                             final ComentarioAdapter comentarioAdapter = new ComentarioAdapter(comentarioList);
                             lstComennts.setAdapter(comentarioAdapter);
+                            setListViewHeightBasedOnChildren(lstComennts);
                         }
                     }
                 });
@@ -92,7 +99,6 @@ public class FragmentDetallePuesto extends Fragment{
                 editComment.setText("");
             }
         });
-
 
         return rootView;
     }
@@ -119,6 +125,30 @@ public class FragmentDetallePuesto extends Fragment{
     {
         byte[] decodedBytes = Base64.decode(input, 0);
         return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = listView.getPaddingTop() + listView.getPaddingBottom();
+
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            if (listItem instanceof ViewGroup) {
+                listItem.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
     }
 
 }
