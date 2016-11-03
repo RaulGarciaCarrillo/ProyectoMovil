96a9e86046e80e3946907b9cd59fe36de13368ba
Raul <ragc_96@hotmail.com>
2016-11-02 23:59:36 -0600

    Pantalla detalle puesto

--- app/src/main/java/com/lmad/proyectomovil/Fragments/FragmentDetallePuesto.java
+++ app/src/main/java/com/lmad/proyectomovil/Fragments/FragmentDetallePuesto.java
@@ -101,10 +101,15 @@ public class FragmentDetallePuesto extends Fragment{
 
         ChargeFavorite();
 
-        if(checkedFavorite==1)
+        if(checkedFavorite==1) {
             checkFavorite.setChecked(true);
-        else
+            checkFavorite.setButtonDrawable(R.mipmap.ic_corazon_rojo);
+        }
+        else {
             checkFavorite.setChecked(false);
+            checkFavorite.setButtonDrawable(R.mipmap.ic_corazonvacio);
+
+        }
 
 
         checkFavorite.setOnClickListener(new View.OnClickListener() {
@@ -114,12 +119,13 @@ public class FragmentDetallePuesto extends Fragment{
                     new Networking(rootView.getContext()).execute("agregarFavorito",1,puesto.getId());
                     checkedFavorite=1;
                     Toast.makeText(getContext(), "Agregado a favoritos.", Toast.LENGTH_SHORT).show();
+                    checkFavorite.setButtonDrawable(R.mipmap.ic_corazon_rojo);
                 }
                else {
                     new Networking(rootView.getContext()).execute("eliminarFavorito",1,puesto.getId());
                     checkedFavorite=0;
                     Toast.makeText(getContext(), "Eliminado de favoritos.", Toast.LENGTH_SHORT).show();
-
+                    checkFavorite.setButtonDrawable(R.mipmap.ic_corazonvacio);
                 }
 
             }
@@ -229,10 +235,14 @@ public class FragmentDetallePuesto extends Fragment{
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if(checkedFavorite==1)
+                        if(checkedFavorite == 1) {
                             checkFavorite.setChecked(true);
-                        else
+                            checkFavorite.setButtonDrawable(R.mipmap.ic_corazon_rojo);
+                        }
+                        else {
                             checkFavorite.setChecked(false);
+                            checkFavorite.setButtonDrawable(R.mipmap.ic_corazonvacio);
+                        }
                     }
                 });
             }
