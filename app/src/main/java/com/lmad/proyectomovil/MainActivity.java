fe30925a7c03349a690afa92133fe8ca4b02d593
Raul <ragc_96@hotmail.com>
2016-11-06 21:56:57 -0600

    Main

--- app/src/main/java/com/lmad/proyectomovil/MainActivity.java
+++ app/src/main/java/com/lmad/proyectomovil/MainActivity.java
@@ -1,6 +1,11 @@
 package com.lmad.proyectomovil;
 
 import android.content.pm.ActivityInfo;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -8,16 +13,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-
 import android.support.v7.app.ActionBarDrawerToggle;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
-
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.lmad.proyectomovil.Fragments.FragmentListaFavoritos;
@@ -25,44 +25,35 @@ import com.lmad.proyectomovil.Fragments.FragmentLogin;
 import com.lmad.proyectomovil.Fragments.FragmentMenuPrincipal;
 import com.lmad.proyectomovil.Fragments.FragmentPerfil;
 
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements DrawerLocker{
 
+public class MainActivity extends AppCompatActivity implements DrawerLocker, GestureOverlayView.OnGesturePerformedListener {
     public DrawerLayout drawerLayout;
-
     private ActionBarDrawerToggle mDrawerToggle;
     private CharSequence mDrawerTitle;
     private CharSequence mTitle;
-
+    private GestureLibrary libreria;
     NavigationView navigationView;
-
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
-
         setContentView(R.layout.activity_main);
+        libreria = GestureLibraries.fromRawResource(this, R.raw.gestures);
+        if(!libreria.load()){
+            //finish();
+        }
+        GestureOverlayView gestureOverlayView = (GestureOverlayView) findViewById(R.id.gestures);
+        gestureOverlayView.addOnGesturePerformedListener(this);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        //
-
-
         navigationView = (NavigationView) findViewById(R.id.nav_viewer);
-
-
         android.app.Fragment existingFragment = getFragmentManager().findFragmentById(android.R.id.content);
 
         if (existingFragment == null) {
-
             changeFragment(new FragmentLogin(), "login");
         }
 
@@ -97,16 +88,11 @@ public class MainActivity extends AppCompatActivity implements DrawerLocker{
         }
 
         if (getSupportFragmentManager().findFragmentByTag(tag) == null) {
-
             FragmentTransaction ft = fm.beginTransaction(); //abrir una transicion (agregar, quitar o reemplazar)
-
             //ft.addToBackStack(null); //no regresar al último fragmento
             ft.replace(R.id.frame_container, fragment, tag); //(id, fragmento)
-
             ft.commit();//cerrar conexión
-
         }
-
     }
 
     @Override
@@ -122,5 +108,12 @@ public class MainActivity extends AppCompatActivity implements DrawerLocker{
         Toast.makeText(this, "Refrescar", Toast.LENGTH_SHORT).show();
     }
 
-
+    @Override
+    public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+        ArrayList<Prediction> predictions = libreria.recognize(gesture);
+        if (predictions.size() > 0 && predictions.get(0).score > 1) {
+            String action = predictions.get(0).name;
+            Toast.makeText(this, action, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
